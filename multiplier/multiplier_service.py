96b23bc50508e46c65e40260393c685d173bfcdc
bigblackbug <bigblackbugg@gmail.com>
2017-06-25 20:48:15 +0300

    minor cleanups

--- multiplier/multiplier_service.py
+++ multiplier/multiplier_service.py
@@ -1,13 +1,7 @@
-import random
-
 from celery import chord
 
 from celery_config.celery_init import app
 from multiplier.celery_tasks import MultiplierTask, CombinerTask
-
-
-def _get_random_string():
-    return str(random.randint(1, 1000))
 
 
 def multiply(first, second):
@@ -37,7 +31,7 @@ def get_result(job_id: str):
     """
     returns status of the job and the result matrix if the computation succeeded
     :param job_id:
-    :return: tuple status->"", result->[[],[]..]
+    :return: tuple (status->"", result->[[],[]..])
     """
     async_result = app.AsyncResult(job_id)
     result = None

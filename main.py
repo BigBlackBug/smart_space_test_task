2802c0905fec20ad87c26e168b5604f9479907bb
bigblackbug <bigblackbugg@gmail.com>
2017-06-25 20:58:27 +0300

    fixed SERVER_PORT not being converted to int

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from web import app
 if __name__ == '__main__':
     application = app.init(routes=routes)
 
-    port = os.environ.get("SERVER_PORT")
+    port = int(os.environ.get("SERVER_PORT"))
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
     web.run_app(application, port=port)

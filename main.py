03730ba2a78f230696def0788c32e409f2697036
bigblackbug <bigblackbugg@gmail.com>
2017-07-05 23:30:41 +0300

    fixed server_port conversion to int

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ from web import app
 if __name__ == '__main__':
     application = app.init(routes=routes)
 
-    port = os.environ.get("SERVER_PORT")
+    port = int(os.environ.get("SERVER_PORT"))
     web.run_app(application, port=port)
